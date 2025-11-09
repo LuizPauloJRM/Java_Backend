86389dc2eb3871228e3880b790ec47de1205d91e
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-09 12:03:12 -0300

    tipos primitivos

---
+++ PadraoDesenvolvimentoJava/src/TiposPrimitivos/Tipo.java
@@ -0,0 +1,11 @@
+package TiposPrimitivos;
+
+public class Tipo {
+	int number = 1;
+	long cpf=1234567891011l;
+	float altura=1.2f;
+	double peso=52.6d;
+	//Aspas simples para caractere 
+	char caractere ='a';
+	boolean simOunao=true;
+}
